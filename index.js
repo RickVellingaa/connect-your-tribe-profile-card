17acdb6b31bd404381b2ca10a80cbda132a7a103
RickVellingaa <rickvellinga2408@gmail.com>
2023-02-08 12:00:32 +0100

    API gebruikt

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Importeer express uit de node_modules map
 import express from 'express'
 
+const url = 'https://whois.fdnd.nl/api/v1/member/rick-vellinga'
+const data = await fetch(url).then((response) => response.json())
+
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -14,7 +17,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -25,3 +28,5 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+console.log(data);
